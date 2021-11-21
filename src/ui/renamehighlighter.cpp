2b83161a089b04b9e150435f328c31e12a12dc81
kaniol-lck <375520360@qq.com>
2021-11-21 14:19:25 +0800

    syntax highlight

--- src/ui/renamehighlighter.cpp
+++ src/ui/renamehighlighter.cpp
@@ -11,6 +11,7 @@ RenameHighlighter::RenameHighlighter(QTextDocument *parent) : QSyntaxHighlighter
 
 void RenameHighlighter::highlightBlock(const QString &text)
 {
+    //bracket
     auto i = QRegularExpression("<|>").globalMatch(text);
     while (i.hasNext()){
         QTextCharFormat format;
@@ -18,6 +19,7 @@ void RenameHighlighter::highlightBlock(const QString &text)
         QRegularExpressionMatch match = i.next();
         setFormat(match.capturedStart(), match.capturedLength(), format);
     }
+    //main pattern
     for(auto &&str : QStringList{ "filename", "id", "version", "name", "tags", "replace" }){
         i = QRegularExpression("<(" + str + ")(\\||>)").globalMatch(text);
         while (i.hasNext()){
@@ -27,8 +29,9 @@ void RenameHighlighter::highlightBlock(const QString &text)
             setFormat(match.capturedStart(1), match.capturedLength(1), format);
         }
     }
+    //tag category
     for(const auto &category : TagCategory::PresetCategories){
-        i = QRegularExpression("\\|(" + category.id() + ")?(\\|(.+?))?>").globalMatch(text);
+        i = QRegularExpression("<tags\\|(" + category.id() + ")?(\\|(.+?))?>").globalMatch(text);
         while (i.hasNext()){
             QTextCharFormat format1;
             format1.setForeground(Qt::darkCyan);
@@ -40,6 +43,24 @@ void RenameHighlighter::highlightBlock(const QString &text)
             setFormat(match.capturedStart(3), match.capturedLength(3), format2);
         }
     }
+    //filename/replace
+    for(auto &&str : QStringList{ "filename", "replace" }){
+        i = QRegularExpression("<" + str + "\\|(.+?)>").globalMatch(text);
+        while (i.hasNext()){
+            QTextCharFormat format1;
+            format1.setForeground(Qt::darkCyan);
+            format1.setFontItalic(true);
+            QTextCharFormat format2;
+            format2.setForeground(Qt::darkYellow);
+            QRegularExpressionMatch match = i.next();
+            int capturedStart = match.capturedStart(1);
+            for(auto &&s : match.captured(1).split("|")){
+                setFormat(capturedStart, s.length(), format2);
+                capturedStart += s.length() + 1;
+            }
+        }
+    }
+    //%1 placeholder
     i = QRegularExpression("%1").globalMatch(text);
     while (i.hasNext()){
         QTextCharFormat format;
