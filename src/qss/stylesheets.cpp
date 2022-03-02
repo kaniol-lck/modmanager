4c88ad2cfa9854fc0e4419668bd9eee74c16f56a
kaniol-lck <375520360@qq.com>
2022-03-02 10:36:54 +0800

    customize qss

--- src/qss/stylesheets.cpp
+++ src/qss/stylesheets.cpp
@@ -9,6 +9,7 @@ const QString &styleSheetsPath()
 {
     static auto path = QDir(QStandardPaths::writableLocation(QStandardPaths::AppConfigLocation))
             .absoluteFilePath("stylesheets");
+    QDir().mkpath(path);
     return path;
 }
 
@@ -25,7 +26,8 @@ const QMap<QString, QString> &builtinStyleSheets()
 QMap<QString, QString> styleSheets()
 {
     auto styleSheets = builtinStyleSheets();
-    for(auto &&fileInfo : QDir(styleSheetsPath()).entryInfoList())
+    auto &&fileInfoList = QDir(styleSheetsPath()).entryInfoList();
+    for(auto &&fileInfo : fileInfoList)
         if(fileInfo.suffix() == "qss"){
             auto name = fileInfo.baseName();
             if(!styleSheets.contains(name))
@@ -39,7 +41,15 @@ QString styleSheetPath(const QString &name)
     if(!styleSheets().contains(name))
         return QString("file:///:/stylesheet/%1.qss").arg("basic");
     auto fileName = QDir(styleSheetsPath()).absoluteFilePath(name + ".qss");
-    if(!QFileInfo(fileName).exists() && builtinStyleSheets().contains(name))
-        return QString("file:///:/stylesheet/%1.qss").arg(name);
-    return fileName;
+    if(!QFileInfo::exists(fileName) && builtinStyleSheets().contains(name)){
+        QFile builtinFile(QString(":/stylesheet/%1.qss").arg(name));
+        QFile newFile(fileName);
+        if(builtinFile.open(QIODevice::ReadOnly) && newFile.open(QIODevice::WriteOnly)){
+            newFile.write("/* This file is auto generated from built-in stylesheet\n"
+                          " * you can create you own qss based on this file.\n"
+                          " */\n\n");
+            newFile.write(builtinFile.readAll());
+        }
+    }
+    return fileName.prepend("file:///");
 }
